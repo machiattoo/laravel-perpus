b17b49020356f62c03b195d898dd452c7d9201a6
machiattoo <bimanyunugrohofirmansah@gmail.com>
2021-12-02 23:06:24 +0700

    Project laravel perpustakaan

---
+++ README.md
@@ -0,0 +1,9 @@
+# Laravel Perpustakaan
+
+This project using laravel 8 framework and plugins some like :
+
+- Sweet Alert : https://github.com/realrashid/sweet-alert
+- Convert Excell : https://github.com/SpartnerNL/Laravel-Excel
+- Convert PDF : https://github.com/barryvdh/laravel-dompdf
+
+Thanks U :)
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       \App\User::insert([
+            [
+              'id'  			=> 1,
+              'name'  			=> 'Admin Aja',
+              'username'		=> 'admin123',
+              'email' 			=> 'admin@admin.com',
+              'password'		=> bcrypt('admin123'),
+              'gambar'			=> NULL,
+              'level'			=> 'admin',
+              'remember_token'	=> NULL,
+              'created_at'      => \Carbon\Carbon::now(),
+              'updated_at'      => \Carbon\Carbon::now()
+            ],
+            [
+              'id'  			=> 2,
+              'name'  			=> 'User Aja',
+              'username'		=> 'user123',
+              'email' 			=> 'user@user.com',
+              'password'		=> bcrypt('user123'),
+              'gambar'			=> NULL,
+              'level'			=> 'user',
+              'remember_token'	=> NULL,
+              'created_at'      => \Carbon\Carbon::now(),
+              'updated_at'      => \Carbon\Carbon::now()
+            ]
+        ]);
+    }
+}
